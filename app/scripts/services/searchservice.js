0ae5b472d40397e31987dd62fa10d7214eaf980c
Adam <adambwinn@gmail.com>
2014-05-24 12:22:22 -0600

    added http to the get request searchservice

--- app/scripts/services/searchservice.js
+++ app/scripts/services/searchservice.js
@@ -8,7 +8,7 @@ angular.module('motoApp')
 
             var deferred = $q.defer();
 
-            $http({method: 'GET', url: 'www.utahmotomarket.com/search/'})   // for local host 'http://localhost:9001/search/'
+            $http({method: 'GET', url: 'http://www.utahmotomarket.com/search/'})   // for local host 'http://localhost:9001/search/'
                 .success(function(data){
                     deferred.resolve(data);
                 }).error(function(err) { console.log(err) });
