3d6f48e728d477f7e24667f012356900715e9121
Adam <adambwinn@gmail.com>
2014-06-03 19:50:45 -0600

    installed passport and cookie parser required passport and local strategy

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require('express');
 
 var mongoose = require('mongoose');
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
 
 var api = require('./api');
 
 bodyParser = require('body-parser');
+
+
+    // define the passport strategies  //
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
 
 
 // start up express app
@@ -15,6 +35,7 @@ app.use(express.static( __dirname + '/app'));
 
 // body parser, is it ever not necessary?
 app.use(bodyParser());
+
 
 
 
